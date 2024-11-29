0c4d327c2ea2d7c6cb0dfcba901e74c53fc9d375
parksangseok <>
2024-11-29 13:38:41 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -18,4 +18,28 @@ $(function () {
     $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
   });
 
+  //윈도우 OS 폰트 설정
+  function windowOS() {
+    let isWindows = navigator.platform.indexOf('Win') > -1;
+    if (isWindows) {
+      $('body').addClass('windowOS');
+    }
+    $('.windowOS *').each(function () {
+      var fontWeight = $(this).css('font-weight');
+      if (fontWeight === '700') {
+        $(this).css({
+          'font-weight': '500',
+          'transform': 'rotate(0.03deg)'
+        });
+      }
+      if (fontWeight === '500') {
+        $(this).css({
+          'font-weight': '400',
+          'transform': 'rotate(0.03deg)'
+        });
+      }
+    });
+  }
+  windowOS()
+
 });
