3d3c047313c280fb5746eeb1cf0c9a454d78ee69
parksangseok <>
2024-11-29 11:54:32 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -8,9 +8,14 @@ $(function () {
 
   });
 
-  $('#cardWrap .cardBox').click(function() {
-    $(this).toggleClass('active'); // Toggle the 'active' class on click
-});
+  /* 카드 */
+  $('#cardWrap .cardBox').click(function () {
+    $(this).toggleClass('active');
+  });
 
+  /* 클릭 시 섹션 이동 */
+  $('.floorInfoBox .floorInfo li a').click(function () {
+    $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+  });
 
 });
